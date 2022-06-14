fb14b82bf67e4e27a89c759278ab7bf359a09b4e
Isaac Poole <isaac@switcherstudio.com>
2022-06-14 00:19:34 -0400

    comments out plan

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
+const core = require("@actions/core");
+const github = require("@actions/github");
 
 try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
+  const path = core.getInput("path");
+
+  // get version number from file
+
+  // bump patch version
+
+  // write new version back to file
+
+  // commit change
+
+  // output new version
+
+  core.setOutput("content", time);
 } catch (error) {
   core.setFailed(error.message);
 }
